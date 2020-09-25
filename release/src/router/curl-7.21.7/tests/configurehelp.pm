b95773841570ed1715a889b8d52c16866a52cc5e
chengxudong2020 <872603935@qq.com>
2020-09-25 08:53:31 +0800

    add
    add

---
+++ release/src/router/curl-7.21.7/tests/configurehelp.pm
@@ -0,0 +1,23 @@
+# This is a generated file.  Do not edit.
+
+package configurehelp;
+
+use strict;
+use warnings;
+use Exporter;
+
+use vars qw(
+    @ISA
+    @EXPORT_OK
+    $Cpreprocessor
+    );
+
+@ISA = qw(Exporter);
+
+@EXPORT_OK = qw(
+    $Cpreprocessor
+    );
+
+$Cpreprocessor = 'mipsel-uclibc-gcc -E -I/home/RT-AC54U/asuswrt/release/src-ra-mt7620/router/openssl/include -I/home/RT-AC54U/asuswrt/release/src-ra-mt7620/router/openssl/include/openssl';
+
+1;
---
+++ release/src/router/shared/version.h
@@ -0,0 +1,10 @@
+#ifndef RTVERSION
+#define RT_MAJOR_VERSION "3.0"
+#define RT_MINOR_VERSION "0.4"
+#define RT_VERSION "3.0.0.4"
+#define RT_SERIALNO "380"
+#define RT_EXTENDNO "PHICOMM-K2"
+#define RT_SWPJVERNO ""
+#define RT_BUILD_NAME "RT-AC54U"
+#define RT_BUILD_INFO "Mon May 21 00:07:27 UTC 2018 root@"
+#endif
